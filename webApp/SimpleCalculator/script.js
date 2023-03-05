0bc282fb22111ae3ab2fe2d041580d04ba700ff8
Aiman Afiq <aimanafiq05@gmail.com>
2023-03-05 17:47:28 +0800

    Another Dumb mistake

--- webApp/SimpleCalculator/script.js
+++ webApp/SimpleCalculator/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const buttonsEl = document.querySelectorAll(".button");
-let num1, num2;
 const resultEl = document.getElementById("result");
+let num1, num2;
 
 let calculate = {
   "+": function (num1, num2) {
@@ -20,9 +20,9 @@ let calculate = {
 };
 
 buttonsEl.forEach((button) => {
-  num1 = parseFloat(document.getElementById("num1").value);
-  num2 = parseFloat(document.getElementById("num2").value);
   button.addEventListener("click", (btnEl) => {
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
     resultEl.textContent = calculate[btnEl.target.textContent](num1, num2);
   });
 });
